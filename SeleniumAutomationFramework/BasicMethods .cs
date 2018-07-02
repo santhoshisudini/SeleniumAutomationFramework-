dd70d29e10889aa7ac1309c28de677cf75986531
santhoshiksr <santhoshir9@gmail.com>
2018-07-02 17:19:08 +0530

    Added Reporting

--- SeleniumAutomationFramework/BasicMethods .cs
+++ SeleniumAutomationFramework/BasicMethods .cs
@@ -17,5 +17,15 @@ namespace SeleniumAutomationFramework
         {
             driver.FindElement(element).SendKeys(keys);
         }
+        public static string Capture(IWebDriver driver, string screenShotName)
+        {
+            ITakesScreenshot ts = (ITakesScreenshot)driver;
+            Screenshot screenshot = ts.GetScreenshot();
+            string pth = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
+            string finalpth = pth.Substring(0, pth.LastIndexOf("bin")) + "ErrorScreenshots\\" + screenShotName + ".png";
+            string localpath = new Uri(finalpth).LocalPath;
+            screenshot.SaveAsFile(localpath, ImageFormat.Png);
+            return localpath;
+        }
     }
 }
